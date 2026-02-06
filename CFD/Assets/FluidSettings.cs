20ba14ea9fa7a3c1c057ab934976af5ceff1c5fa
TheWebb94 <alexwebb94@hotmail.co.uk>
2026-02-06 17:12:44 +0000

    added smoothing r to fluid settings

--- CFD/Assets/FluidSettings.cs
+++ CFD/Assets/FluidSettings.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class FluidSettings : MonoBehaviour
 {
-    public float gravity = 9.81f;
+    public float gravity = -9.81f;
     [Range(0f, 1f)] public float collisionDamping = 0.9f;
     public float particleSize = 1f;
-   
+    public float smoothingRadius = 4f;
     [HideInInspector] public int version;
 
     private void OnValidate()
