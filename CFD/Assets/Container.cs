b42f4d3a429e95c0719939a0248023ac29a3973b
TheWebb94 <alexwebb94@hotmail.co.uk>
2026-02-13 19:59:09 +0000

    pls help

--- CFD/Assets/Container.cs
+++ CFD/Assets/Container.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class Container : MonoBehaviour
 {
     public Vector2 boundsSize = new Vector2(20, 20);
-    public FluidSettings settings;
+    public FluidSimulation fluidSimulation;
+    
+    
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        float r = settings.particleSize / 2;
+        float r = fluidSimulation.particleSize / 2;
         // Draw centered on THIS object
         Gizmos.DrawWireCube(transform.position, new Vector2((float)boundsSize.x - r, (float)boundsSize.y - r));
     }
---
+++ CFD/Assets/Particle.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    public Vector2 position;
+    public Vector2 velocity;
+    public Vector2 pressure;
+    public float density;
+}
